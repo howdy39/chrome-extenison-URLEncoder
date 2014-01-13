71a16b8846c39dc78569a040340641d56e802ef6
howdy39 <tatsuya.howdy@gmail.com>
2014-01-13 16:51:14 +0900

    v1.0.2
    A value is chosen after changing.

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,11 +2,11 @@ $(function() {
 	$('#encode-button').click(function() {
 		var beforeText = $('#encode-target-text').val();
 		var afterText = encodeURIComponent(beforeText);
-		$('#decode-target-text').val(afterText);
+		$('#decode-target-text').val(afterText).select();
 	});
 	$('#decode-button').click(function() {
 		var beforeText = $('#decode-target-text').val();
 		var afterText = decodeURIComponent(beforeText);
-		$('#encode-target-text').val(afterText);
+		$('#encode-target-text').val(afterText).select();
 	});
 });
