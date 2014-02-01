e04b90df6cdecfd8f48e48449dd7d7684dc71e1e
howdy39 <tatsuya.howdy@gmail.com>
2014-02-01 18:58:49 +0900

    v1.1.0

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,12 +1,64 @@
 $(function() {
+	init();
+	function init() {
+		// focus bug? @see http://blog.howdylikes.jp/entry/2014/02/01/160505
+		if(location.search !== "?foo") {
+			location.search = "?foo";
+			throw new Error;
+		}
+		$('#encode-target-text').focus();
+
+		var encodeType = localStorage.getItem('encodeType');
+		if (encodeType) {
+			$('#encode-type').val(encodeType);
+		}
+	}
+
+	$('#encode-type').change(function () {
+		var encodeType = $('#encode-type').val();
+		localStorage.setItem('encodeType', encodeType);
+	});
+
 	$('#encode-button').click(function() {
 		var beforeText = $('#encode-target-text').val();
-		var afterText = encodeURIComponent(beforeText);
+		var afterText = encode(beforeText);
 		$('#decode-target-text').val(afterText).select();
 	});
+
 	$('#decode-button').click(function() {
 		var beforeText = $('#decode-target-text').val();
-		var afterText = decodeURIComponent(beforeText);
+		var afterText = decode(beforeText);
 		$('#encode-target-text').val(afterText).select();
 	});
+
+	function encode(beforeText) {
+		var encodeType = localStorage.getItem('encodeType');
+
+		if (encodeType === 'encodeURI') {
+			return encodeURI(beforeText);
+		}
+
+		if (encodeType === 'encodeURIComponent') {
+			return encodeURIComponent(beforeText);
+		}
+
+		if (encodeType === 'escape') {
+			return escape(beforeText);
+		}
+	}
+	function decode(beforeText) {
+		var encodeType = localStorage.getItem('encodeType');
+		
+		if (encodeType === 'encodeURI') {
+			return decodeURI(beforeText);
+		}
+
+		if (encodeType === 'encodeURIComponent') {
+			return decodeURIComponent(beforeText);
+		}
+
+		if (encodeType === 'escape') {
+			return unescape(beforeText);
+		}
+	}
 });
